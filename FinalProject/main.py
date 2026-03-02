03556825c2f685f4d414dd096264d88e7cbaec20
PB-5539 <behaggp5539@student.faytechcc.edu>
2026-03-02 21:59:27 +0000

    created and imported modules to main.py

---
+++ FinalProject/main.py
@@ -0,0 +1,16 @@
+#Parker Behagg
+#
+#FinalProject
+# Create a game using the numpy and random modules and use loops and functions (include a main() function to run the program).
+#----import modules
+import misc_utils as misc
+import random as rndm
+import math as mt
+import tkinter as tk
+from tkinter import ttk
+
+#----initialize variables
+
+#Main function
+def main():
+    print("game now loading...")
