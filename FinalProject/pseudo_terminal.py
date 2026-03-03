16b826c4be00cffc7c971870114d23329ef64d1f
PB-5539 <behaggp5539@student.faytechcc.edu>
2026-03-03 15:57:06 +0000

    created a pseudo-terminal python file

---
+++ FinalProject/pseudo_terminal.py
@@ -0,0 +1,16 @@
+import math as mt
+import random as rdm
+import time as tm
+import misc_utils as misc
+import ui
+import events as ev
+import tasks as tsk
+import alerts as als
+import winsound as ws
+import tkinter as tk
+from tkinter import ttk
+
+def create_terminal(geometry, resizeable, title):
+    print(f"new terminal created with title: {title}, geometry: {geometry}, and a resizeable condition of: {resizeable}")
+def terminal_logic():
+    print("terminal logic ran once")
