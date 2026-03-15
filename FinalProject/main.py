7d59491aea5f46111e3796ec54dd259972d14c4f
PB-5539 <behaggp5539@student.faytechcc.edu>
2026-03-15 17:47:47 -0400

    made some changes, alignment logic is bugged

--- FinalProject/main.py
+++ FinalProject/main.py
@@ -48,9 +48,13 @@ def main():
     ui.add_button("settings", ls_root[0], 20, 20, lambda: ui.settings(ls_root[1]), dict_buttons)
     ui.add_button("quit", ls_root[0], 20, 120, lambda: ui.quitgame(ls_root[0]), dict_buttons)
 
-    ui.add_label("gamewindowlabel", ls_root[2], 20, 20, f"------------------")
+    ui.add_label("gamewindowlabel", ls_root[2], 20, 20, f"------------------", dict_labels, "None", "None")
     ui.add_button("terminal", ls_root[2], 20, 20, lambda: pt.show(ls_root, ls_terminal), dict_buttons)
-    ui.add_frame("sidebar", ls_root[2], 20, 20, dict_frames)
+    ui.add_frame("sidebar", ls_root[2], 20, 20, dict_frames, "Right", "None", "red")
+    ui.add_label("sidebar title", dict_frames["sidebar"], 20, 20, "sidebar!",  dict_labels, "Right", "None")
+    #TO DO: 
+    #fix the left/right alignment logic for the modular tkinter ui system
+    #add top/botton alignment logic
 
 
 
