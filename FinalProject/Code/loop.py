96e0741d8ad3ce08fa668f92cc2cde5b6136570b
PB-5539 <behaggp5539@student.faytechcc.edu>
2026-03-17 11:38:50 -0400

    misc changes

--- FinalProject/Code/loop.py
+++ FinalProject/Code/loop.py
@@ -16,39 +16,9 @@ import pseudo_terminal as pt
 def loop(ls_root, ls_terminal, dict_buttons, dict_entries, dict_frames, dict_labels, dict_sliders, dict_vars):
     while True:
         print(ls_root, ls_terminal, dict_buttons, dict_entries, dict_frames, dict_labels, dict_sliders, dict_vars)
-    #Main Game Logic
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
+    #Main Game Logic---------------------\/
+    print("game logic")
+    #Main Game logic---------------------/\
 def begin_loop(ls_threads, ls_root, ls_terminal, dict_buttons, dict_entries, dict_frames, dict_labels, dict_sliders, dict_vars):
     thread = th.Thread(target=lambda: loop(ls_root, ls_terminal, dict_buttons, dict_entries, dict_frames, dict_labels, dict_sliders, dict_vars),  daemon=True)
     thread.start()
