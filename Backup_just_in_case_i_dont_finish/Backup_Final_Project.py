37ffe9b0396b26b6e580a2fd43c9c7b6d11647ec
PB-5539 <behaggp5539@student.faytechcc.edu>
2026-03-26 09:59:11 -0400

    woking on a tutorial for the backup

--- Backup_just_in_case_i_dont_finish/Backup_Final_Project.py
+++ Backup_just_in_case_i_dont_finish/Backup_Final_Project.py
@@ -9,11 +9,16 @@ def main(dict_player_info):
     ls_items = ["ominous horn", "shattered orb", "mysterious potion", "necromancer staff", "red orb", "glowing scroll", ]
     attempts = 0
     player_stats = {"name":dict_player_info["name"],"gold":0, "health":10, "arcane_dust":0}
-    dict_encounters = {"elf":["dialogue", "dialogue", "+8 gold"], "placeholder":["dialogue list...", "...", "reward"]}
+    ls_elf = ["","--An elf wanders into your shop--","", "she asks if you have any grimoires.", "you point over to the back of the shop","'There's probably not anything interesting, but they're over there.","the elf grabs a few, pays, and leaves","+8 gold"]
+    dict_encounters = {"elf":ls_elf, "placeholder":["dialogue list...", "...", "reward"]}
 
     while (player_stats["health"] > 0) and (player_stats["gold"] >=0 ):
-        player_stats["gold"] = player_stats["gold"] - 1 #THIS IS A PLACEHOLDER FOR THE GAME LOGIC
-    #on death/bankrupcy logic goes here
+        if tutorial_done:
+            print()
+        else:
+            tutorial()
+            tutorial_done = True
+        
     if (player_stats["gold"] < 0) and (player_stats["health"] > 0):
         print()
         print()
@@ -50,10 +55,13 @@ def main(dict_player_info):
 
 def intro():
     dict_player_info = {"name":"", }#idk what other choices to add
-    ls_intro = []
+    ls_intro = ["You are an appraiser of magical artifacts, trusted by mages, merchants, and the occasional adventurer.","In your modest shop, curious items appear almost daily—some harmless, some… less so.","Each choice matters: one wrong touch, one misread rune, and disaster could strike.","Gold is scarce, dust is precious, and rumors of rare, legendary artifacts are whispered across the land."]
+    print()
+    print()
+    print()
     for i in range(len(ls_intro)):
         print(ls_intro[i])
-        tm.sleep(2)
+        tm.sleep(5)
     for i in range(100):    #clears the area a bit
         print()
     print("====================================================================")
@@ -99,5 +107,19 @@ def overview(player_stats):
     name = player_stats["name"]
     overview = f"====overview====\n{'Name:':<12}{name}\n{'Gold:':<12}{gold:<12}\n{'Health:':<12}{health:<12}\n"
     return overview
-
+def tutorial():
+    print()
+    print()
+    print("Alright, welcome to your new shop.")
+    print("Oh! Your first customer!")
+    print()
+    print("--someone enters the shop--")
+    print()
+    print("Customer: Hey, I found this wierd orb while exploring. Can you do anything with it?")
+    print("====================================================================================")
+    print(f"What would you like to do? | 1. Accept (-2 Gold)    2. Decline (-2 Reputation)")
+    print("====================================================================================")
+    decision = input("Choice: ")
+    if decision in [""]:
+        print("choice: YES")
 main(intro())
